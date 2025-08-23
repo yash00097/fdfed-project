cc7b0bfc104bb5ce18176b187dde24cece7baf85
yash00097 <yashwanthvuppala77@gmail.com>
2025-08-23 20:18:39 +0530

    signout reduces added

--- frontend/src/redux/user/userSlice.js
+++ frontend/src/redux/user/userSlice.js
@@ -23,6 +23,18 @@ const userSlice = createSlice({
             state.loading = false;
             state.error = action.payload;
         },
+        signOutStart: (state) => {
+            state.loading = true;
+        },
+        signOutSuccess: (state) => {
+            state.currentUser = null;
+            state.loading = false;
+            state.error = null;
+        },
+        signOutFailure: (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
+        },
     }
 })
 
