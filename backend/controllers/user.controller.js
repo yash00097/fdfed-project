923d8043e0f43d6400f9e5e68640421acd26ea50
siddu <annamsiddeshwereddy2006@gmail.com>
2025-08-24 16:07:58 +0530

    cloudinary setup & basic profile page completed

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,55 +1,77 @@
-import {errorHandler} from "../utils/error.js";
+import { errorHandler } from "../utils/error.js";
 import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
 
-
+/**
+ * Update user info: username, email, password, avatar
+ */
 export const updateUser = async (req, res, next) => {
-    if (req.user.id !== req.params.id) {
-        return next(errorHandler(401, 'You can only update your own account!'));
+  // Ensure the user is updating their own account
+  if (req.user.id !== req.params.id) {
+    return next(errorHandler(403, "You can update only your own account!"));
+  }
+
+  try {
+    // Prepare the fields to update
+    const updateData = {
+      username: req.body.username,
+      email: req.body.email,
+    };
+
+    // Update password if provided
+    if (req.body.password) {
+      const salt = bcrypt.genSaltSync(10);
+      updateData.password = bcrypt.hashSync(req.body.password, salt);
     }
-    
-    try {
-        const updateData = { ...req.body };
-        
-        if (updateData.password) {
-            updateData.password = bcrypt.hashSync(updateData.password, 10);
-        }
 
-
-        const updatedUser = await User.findByIdAndUpdate(
-            req.params.id,
-            {
-                $set: {
-                    username: updateData.username,
-                    email: updateData.email,
-                    password: updateData.password,
-                    avatar: updateData.avatar
-                }
-            },
-            { new: true }
-        );
-
-        const { password, ...others } = updatedUser._doc;
-        res.status(200).json(others);
-    } catch (err) {
-        next(err);
+    // Update avatar if a file is uploaded
+    if (req.file) {
+      updateData.avatar = req.file.path; // Cloudinary URL
     }
+
+    // Update user in DB
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      { $set: updateData },
+      { new: true }
+    );
+
+    // Exclude password from response
+    const { password, ...others } = updatedUser._doc;
+
+    res.status(200).json({
+      success: true,
+      message: "User updated successfully!",
+      user: others,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
+/**
+ * Delete user account
+ */
 export const deleteUser = async (req, res, next) => {
-    if(req.user.id !== req.params.id){
-        return next(errorHandler(res, 401, 'you can only delete your own account!'));
-    }
-    try {
-        await User.findByIdAndDelete(req.params.id);
-        res.clearCookie("access_token", {
-            httpOnly: true,
-            secure: true,
-            sameSite: "none",
-        });
-        res.status(200).json('User has been deleted successfully!');
-        
-    } catch (error) {
-        next(error);
-    }
-}
+  if (req.user.id !== req.params.id) {
+    return next(errorHandler(403, "You can delete only your own account!"));
+  }
+
+  try {
+    await User.findByIdAndDelete(req.params.id);
+
+    // Clear authentication cookie
+    res.clearCookie("access_token", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "User has been deleted successfully!",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import {updateUser,deleteUser} from '../controllers/user.controller.js';
 import {verifyToken} from "../utils/verifyUser.js";
+import {upload} from "../config/cloudinaryConfig.js";
 
 
 const router = express.Router();
 
-router.post('/update/:id' ,verifyToken, updateUser);
+router.put('/update/:id' ,verifyToken, upload.single("avatar"), updateUser);
 router.delete('/delete/:id' ,verifyToken, deleteUser);
 
 export default router; 
