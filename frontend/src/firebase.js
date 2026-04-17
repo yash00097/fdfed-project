c5dc7701e518fa1ef13ee1e09e18df8a1243173b
siddu <annamsiddeshwereddy2006@gmail.com>
2026-04-18 00:00:52 +0530

    firebase setup matched

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -7,13 +7,12 @@ import { initializeApp } from "firebase/app";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "fdfed-project-a65b6.firebaseapp.com",
-  projectId: "fdfed-project-a65b6",
-  storageBucket: "fdfed-project-a65b6.firebasestorage.app",
-  messagingSenderId: "542160872421",
-  appId: "1:542160872421:web:b560d62be166e35c5e450c",
-  measurementId: "G-T4BKTHWLTR"
+  authDomain: "mern-estate-1a1ba.firebaseapp.com",
+  projectId: "mern-estate-1a1ba",
+  storageBucket: "mern-estate-1a1ba.firebasestorage.app",
+  messagingSenderId: "158505809139",
+  appId: "1:158505809139:web:29312d88b4ae0b83eef386"
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+ export const app = initializeApp(firebaseConfig);
