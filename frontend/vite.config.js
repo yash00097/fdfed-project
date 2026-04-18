357b27e8b20ec456c6393632058dea1adce7dd21
dileep <yarramanenidileep@gmail.com>
2026-04-18 17:05:09 +0530

    deployment of frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  server: {
-    port: 5173,
-    proxy: {
-      '/backend': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    server: {
+      port: 5173,
+      proxy: {
+        '/backend': {
+          target: env.VITE_RENDER_URL,
+          changeOrigin: true,
+          secure: true,
+        },
       },
     },
-  },
-  plugins: [
-    react(),
-    tailwindcss()],
+    plugins: [
+      react(),
+      tailwindcss()
+    ],
+  }
 })
